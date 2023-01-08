a6283307c60238ce8964cda86d323e68ef66f627
Marcos Plata <el.marcoss@gmail.com>
2023-01-08 09:47:58 -0600

    Frontend project creation and setup

---
+++ commits-story-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Commits Story</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
